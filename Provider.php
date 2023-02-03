bc732452bd343490e3f642617a1eeef26dcc1cd9
Jan Ruhfus <janruhfus0@gmail.com>
2023-02-03 15:11:35 +0100

    Adjust userdata endpoint

--- Provider.php
+++ Provider.php
@@ -119,7 +119,7 @@ class Provider extends AbstractProvider
         return rtrim($this->getConfig('host'), '/').'/'.ltrim(($this->getConfig($type, Arr::get([
             'authorize_uri' => 'oauth/authorize',
             'token_uri'     => 'oauth/token',
-            'userinfo_uri'  => 'api/user',
+            'userinfo_uri'  => 'oauth/user',
         ], $type))), '/');
     }
 
